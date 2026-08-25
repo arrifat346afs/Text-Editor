780b8aac51138fd6a0ce9ea5e39b8cfce88c57b3
arrifat346afs <arrifat1235@gmail.com>
2026-08-25 22:51:04 +0600

    feat(app): add tab state management to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { Table } from "lucide-react";
 import "./App.css";
 import FileTab from "./app/navigation/FileTabs";
 import NaviGation from "./app/navigation/NaviGation";
 import TextArea from "./app/TextArea";
+import { useState } from "react";
+
+
+interface tab {
+  id: string,
+  filename: string,
+  content: string,
+  isDirty: boolean,
+}
+
+
+
+
 
 function App() {
+  const [tabs, setTabs] = useState<Tab[]>([
+    { id: "1", filename: "Untitled-1", content: "", isDirty: false },
+  ]);
+ const [activeTabId, setActiveTabId] = useState("1");
+ const activetab = tabs.find((t)=> t.id===activeTabId)
+
+
+
   return (
     <main className="flex flex-col w-full h-full">
       <NaviGation />
